53c3f4814dce46628febcef476335da13812538c
Evan Cole <evanbcole22@gmail.com>
2022-07-09 23:14:01 -0500

    Removed code to create new folder for each query

--- bing_image_downloader/downloader.py
+++ bing_image_downloader/downloader.py
@@ -18,23 +18,23 @@ force_replace=False, timeout=60, filter="", verbose=True):
         adult = 'on'
 
     
-    image_dir = Path(output_dir).joinpath(query).absolute()
+    # image_dir = Path(output_dir).joinpath(query).absolute()
 
-    if force_replace:
-        if Path.isdir(image_dir):
-            shutil.rmtree(image_dir)
+    # if force_replace:
+    #     if Path.isdir(image_dir):
+    #         shutil.rmtree(image_dir)
 
-    # check directory and create if necessary
-    try:
-        if not Path.is_dir(image_dir):
-            Path.mkdir(image_dir, parents=True)
+    # # check directory and create if necessary
+    # try:
+    #     if not Path.is_dir(image_dir):
+    #         Path.mkdir(image_dir, parents=True)
 
-    except Exception as e:
-        print('[Error]Failed to create directory.', e)
-        sys.exit(1)
+    # except Exception as e:
+    #     print('[Error]Failed to create directory.', e)
+    #     sys.exit(1)
         
-    print("[%] Downloading Images to {}".format(str(image_dir.absolute())))
-    bing = Bing(query, limit, image_dir, adult, timeout, filter, verbose)
+    print("[%] Downloading Images to {}".format(str(output_dir.absolute())))
+    bing = Bing(query, limit, output_dir, adult, timeout, filter, verbose)
     bing.run()
 
 
